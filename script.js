bac43c29429f5c6b879292eb623eb90a05ab1268
ksrvvv <kksrvvv@gmail.com>
2022-11-05 15:45:08 +0300

    final for main

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 var skillCardHeaders=document.getElementsByClassName("skill-card-header")
 function expandSkillCard (e){
  var body=document.getElementById(e.id.replace('Button',''))   
-    if(e.firstChild.getAttribute('src')=="add.png"){
+    if(e.firstChild.getAttribute('src')=="../sourse/add.png"){
         
 body.classList.remove("hidden")
- e.firstChild.setAttribute('src', 'minus.png');
+ e.firstChild.setAttribute('src', '../sourse/minus.png');
     }
    
     else {
         body.classList.add("hidden")
-          e.firstChild.setAttribute('src', 'add.png');
+          e.firstChild.setAttribute('src', '../sourse/add.png');
     }
 
 }
 function getToResume(){
     window.open("https://drive.google.com/file/d/1QWKV66HMQ52FxrycfnqQdmH4BJJixlSF/view?usp=drivesdk", '_blank');
 }
+(function($){
+    $(window).on("load",function(){
+        $("nav a").mPageScroll2id();
+    });
+})(jQuery);
+

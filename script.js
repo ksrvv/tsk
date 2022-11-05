0fd2226c9055b5d13188a0ec4f42ee6eca9eab2f
ksrvvv <kksrvvv@gmail.com>
2022-11-05 16:00:42 +0300

    path changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var skillCardHeaders=document.getElementsByClassName("skill-card-header")
 function expandSkillCard (e){
  var body=document.getElementById(e.id.replace('Button',''))   
-    if(e.firstChild.getAttribute('src')=="../sourse/add.png"){
+    if(e.firstChild.getAttribute('src')=="add.png"){
         
 body.classList.remove("hidden")
- e.firstChild.setAttribute('src', '../sourse/minus.png');
+ e.firstChild.setAttribute('src', 'minus.png');
     }
    
     else {
         body.classList.add("hidden")
-          e.firstChild.setAttribute('src', '../sourse/add.png');
+          e.firstChild.setAttribute('src', 'add.png');
     }
 
 }
